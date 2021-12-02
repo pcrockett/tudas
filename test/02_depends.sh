0916fc3a1fc6ff12678eca38ef684be73e59bc11
Philip Crockett <contact@philcrockett.com>
2021-12-02 23:18:28 +0100

    Test multiple @depends_on per goal

--- test/02_depends.sh
+++ test/02_depends.sh
@@ -1,6 +1,9 @@
 @goal a
     echo "Running a"
 
+@goal a2
+    echo "Running a2"
+
 @goal b
 @depends_on a
     echo "Running b"
@@ -22,6 +25,11 @@
 @goal dependency_should_fail
 @depends_on a should_fail
     echo "You should not see this"
+
+@goal multiple_depends
+@depends_on a
+@depends_on a2
+    echo "Running multiple_depends"
 
 @goal default
 @depends_on c d

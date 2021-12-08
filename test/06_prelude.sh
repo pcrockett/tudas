910102dfb008669e823743deac588d0db301ccca
Philip Crockett <contact@philcrockett.com>
2021-12-09 00:06:19 +0100

    Failing test for indentation, whitespace

--- test/06_prelude.sh
+++ test/06_prelude.sh
@@ -2,5 +2,18 @@ A=123
 B=456
 # Comments should stay in the prelude.
 
+C=$(cat << EOF
+Here docs should
+    have their
+    indentation
+    and
+    tailing whitepaces    
+    
+and empty lines
+
+preserved.
+EOF
+)
+
 @goal default
     true
